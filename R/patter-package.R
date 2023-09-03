d681c7aff747cf0c0447090eb15fd7f3f8487916
edwardlavender <el72@st-andrews.ac.uk>
2023-09-03 15:25:24 +0200

    Define imports

---
+++ R/patter-package.R
@@ -0,0 +1,27 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom rlang .data
+#'
+#' @importFrom dplyr mutate
+#' @importFrom dplyr filter
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr slice
+#' @importFrom dplyr arrange
+#' @importFrom dplyr select
+#'
+#' @importFrom dtplyr lazy_dt
+#'
+#' @importFrom data.table :=
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table data.table
+#' @importFrom data.table as.data.table
+NULL
