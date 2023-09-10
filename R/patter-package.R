94e2baf425651cb70d132ad468105f21616d4522
edwardlavender <el72@st-andrews.ac.uk>
2023-09-10 10:51:01 +0200

    Add package-level documentation

--- R/patter-package.R
+++ R/patter-package.R
@@ -1,3 +1,35 @@
+#' @title  [`patter`]: Movement Modelling For Passive Acoustic Telemetry
+#' @description [`patter`] is a re-implementation of the [flapper](https://github.com/edwardlavender/flapper) family of algorithms for passive acoustic telemetry that simpler, faster and better tested than its [predecessor](https://github.com/edwardlavender/flapper).
+#'
+#' # Workflow
+#'
+#' To implement the depth-contour algorithm, use:
+#'
+#' * [`dc()`] to implement the algorithm;
+#'
+#' To implement the acoustic-centroid (AC) or acoustic-container depth-contour (ACDC) algorithms, use:
+#'
+#' * [`acs_setup_obs()`] to set up observations;
+#' * [`acs_setup_detection_containers()`] to set up detection containers;
+#' * [`acs_setup_detection_overlaps()`] to set up detection overlaps;
+#' * [`acs_setup_detection_kernels()`] to set up detection kernels;
+#' * [`ac()`] or [`acdc()`] to implement the algorithm(s);
+#'
+#' To implement particle filtering, use:
+#'
+#' * [`pf_setup_record()`] to set up the record;
+#' * [`pf()`] to implement particle filtering;
+#'
+#' @author Edward Lavender ([ORCID](https://orcid.org/0000-0002-8040-7489))
+#' @seealso
+#' * For information on the [flapper](https://github.com/edwardlavender/flapper) algorithms, see Lavender et al. ([2023](https://doi.org/10.1111/2041-210X.14193)).
+#' * For information on [`patter`]'s predecessor, see \url{https://github.com/edwardlavender/flapper}.
+#' * For further information on  [`patter`], see \url{https://github.com/edwardlavender/patter}.
+#' * For feature requests and bug reports, see \url{https://github.com/edwardlavender/patter/issues}.
+#' * For support, contact [edward.lavender@eawag.ch](mailto:[edward.lavender@eawag.ch).
+#'
+#' @references Lavender, E. et al. (2023). An integrative modelling framework for passive acoustic telemetry. Methods in Ecology and Evolution. \url{https://doi.org/10.1111/2041-210X.14193}.
+#'
 #' @keywords internal
 "_PACKAGE"
 
