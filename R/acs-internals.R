1eaa136e49dd1c0a58ac563e85bfd03c55f15b7f
edwardlavender <el72@st-andrews.ac.uk>
2023-09-11 13:44:08 +0200

    Use dot pronoun in .acs() function arguments

--- R/acs-internals.R
+++ R/acs-internals.R
@@ -45,8 +45,8 @@
 #' @keywords internal
 
 .acs_check_detection_overlaps <- function(.ov) {
-  check_named_list(.ov)
   if (!is.null(.ov)) {
+    check_named_list(.ov)
     check_names(.ov, c("list_by_receiver", "list_by_date"))
   }
 }
@@ -172,18 +172,18 @@
 
 #' @title AC* algorithm back-end
 #' @description This function is the back-end of the acoustic-container and acoustic-container depth-contour algorithms.
-#' @param obs A [`data.table`] with observations, from [`acs_setup_obs()`].
-#' @param bathy A [`SpatRaster`] that defines the grid over which the algorithms are implemented.
-#' @param detection_overlaps A named `list`, from [`acs_setup_detection_overlaps()`].
-#' @param detection_kernels A `list`, from [`acs_setup_detection_kernels()`].
-#' @param save_record,save_cumulative Logical inputs that control options for saving outputs in memory.
-#' * `save_record` defines whether or not to save the record of the possible locations of the individual at each time step in the `record` element of the output;
-#' * `save_cumulative` defines whether or not to save a cumulative (probability-of-use) map, derived from the normalised summation of each element in `record` in the `map` element of the output;
-#' @param write_record A named list, passed to [`terra::writeRaster`], to save the `record` `SpatRaster`s to file at each time step. The `filename` argument should define the directory in which to write files. Files are named by time step (i.e., 1.tif, 2.tif, ..., N.tif).
-#' @param progress A logical variable that defines whether or not to implement a progress bar (via [`progress::progress_bar()`]).
-#' @param prompt A logical variable that defines whether or not a user prompt is required between time steps. If provided, the function plots the possible locations of the individual at each time step. This is useful for diagnostics.
-#' @param verbose A logical variable that defines whether or not to print messages to the console or to file to relay function progress. If `con = ""`, messages are printed to the console; otherwise, they are written to file (see below).
-#' @param con If `verbose = TRUE`, `con` is character string that defines the full pathway to a `.txt` file (which can be created on-the-fly) into which messages are written to relay function progress. This approach, rather than printing to the console, is recommended for clarity, speed and debugging.
+#' @param .obs A [`data.table`] with observations, from [`acs_setup_obs()`].
+#' @param .bathy A [`SpatRaster`] that defines the grid over which the algorithms are implemented.
+#' @param .detection_overlaps A named `list`, from [`acs_setup_detection_overlaps()`].
+#' @param .detection_kernels A `list`, from [`acs_setup_detection_kernels()`].
+#' @param .save_record,.save_cumulative Logical inputs that control options for saving outputs in memory.
+#' * `.save_record` defines whether or not to save the record of the possible locations of the individual at each time step in the `record` element of the output;
+#' * `.save_cumulative` defines whether or not to save a cumulative (probability-of-use) map, derived from the normalised summation of each element in `record` in the `map` element of the output;
+#' @param .write_record A named list, passed to [`terra::writeRaster`], to save the `record` `SpatRaster`s to file at each time step. The `filename` argument should define the directory in which to write files. Files are named by time step (i.e., 1.tif, 2.tif, ..., N.tif).
+#' @param .progress A logical variable that defines whether or not to implement a progress bar (via [`progress::progress_bar()`]).
+#' @param .prompt A logical variable that defines whether or not a user prompt is required between time steps. If provided, the function plots the possible locations of the individual at each time step. This is useful for diagnostics.
+#' @param .verbose A logical variable that defines whether or not to print messages to the console or to file to relay function progress. If `con = ""`, messages are printed to the console; otherwise, they are written to file (see below).
+#' @param .con If `.verbose = TRUE`, `.con` is character string that defines the full pathway to a `.txt` file (which can be created on-the-fly) into which messages are written to relay function progress. This approach, rather than printing to the console, is recommended for clarity, speed and debugging.
 #' @return The function returns an [`ac_record-class`] object.
 #'
 #' @source This function evolved from `.acs()` in the [flapper](https://github.com/edwardlavender/flapper) package. Key developments include:
@@ -192,42 +192,43 @@
 #' * Exploitation of [`data.table`] and [`terra`] for substantially improved speed;
 #' * The use of [`terra::buffer()`] to represent container dynamics, which is faster and removes the polygon versus grid discrepancy in [flapper](https://github.com/edwardlavender/flapper);
 #'
-#' @seealso For internal helpers, see [`.acs_absences()`] and [`.acs_given_detection()`].
+#' @seealso For internal helpers, see `.acs_*` functions.
 #' @author Edward Lavender
-#' @keywords internal
+#' @export
 
-.acs <- function(obs,
-                 bathy,
-                 detection_overlaps = NULL,
-                 detection_kernels,
-                 save_cumulative = FALSE,
-                 save_record = FALSE,
-                 write_record = NULL,
-                 progress = TRUE, prompt = FALSE,
-                 verbose = TRUE, con = "") {
+.acs <- function(.obs,
+                 .bathy,
+                 .detection_overlaps = NULL,
+                 .detection_kernels,
+                 .update_ac = NULL, ...,
+                 .save_cumulative = FALSE,
+                 .save_record = FALSE,
+                 .write_record = NULL,
+                 .progress = TRUE, .prompt = FALSE,
+                 .verbose = TRUE, .con = "") {
 
   #### Check user inputs
   t_onset <- Sys.time()
-  obs <- .acs_check_obs(obs)
-  .acs_check_bathy(bathy)
-  .acs_check_detection_overlaps(detection_overlaps)
-  .acs_check_detection_kernels(detection_kernels, bathy)
-  if (!save_cumulative && !save_record && is.null(write_record)) {
-    abort("`save_cumulative = FALSE`, `save_record = FALSE` and `write_record = NULL`. There is nothing to do.")
+  .obs <- .acs_check_obs(.obs)
+  .acs_check_bathy(.bathy)
+  .acs_check_detection_overlaps(.detection_overlaps)
+  .acs_check_detection_kernels(.detection_kernels, .bathy)
+  if (!.save_cumulative && !.save_record && is.null(.write_record)) {
+    abort("`.save_cumulative = FALSE`, `.save_record = FALSE` and `.write_record = NULL`. There is nothing to do.")
   }
-  write_record_folder <- .acs_check_write_record(write_record)
-  if (!verbose & con != "") {
-    warn("Input to `con` ignores since `verbose = FALSE`.")
+  write_record_folder <- .acs_check_write_record(.write_record)
+  if (!.verbose & .con != "") {
+    warn("Input to `con` ignores since `.verbose = FALSE`.")
   }
 
   #### Set up messages
   # Define log file & function to send messages to the console/file
-  if (verbose && con != "") {
-    create_log(con)
+  if (.verbose && .con != "") {
+    create_log(.con)
   }
-  append_messages <- ifelse(con == "", FALSE, TRUE)
-  cat_to_cf <- function(..., message = verbose, file = con, append = append_messages) {
-    if (message) cat(paste(..., "\n"), file = con, append = append)
+  append_messages <- ifelse(.con == "", FALSE, TRUE)
+  cat_to_cf <- function(..., message = .verbose, file = .con, append = append_messages) {
+    if (message) cat(paste(..., "\n"), file = .con, append = append)
   }
   cat_to_cf(paste0("patter::.acs() called (@ ", t_onset, ")..."))
   on.exit(cat_to_cf(paste0("patter::.acs() call ended (@ ", Sys.time(), ").")), add = TRUE)
@@ -240,22 +241,22 @@
   # * This is solely to stop an RStudio warning flag (past is defined before first use)
   past   <- NULL
   # Define the final detection
-  detection_id_final <- max(obs$detection_id)
+  detection_id_final <- max(.obs$detection_id)
   # Define progress bar
-  if (progress) {
-    pb <- progress::progress_bar$new(total = max(obs$timestep))
+  if (.progress) {
+    pb <- progress::progress_bar$new(total = max(.obs$timestep))
     pb$tick(0)
   }
 
   #### Implement loop
-  for (t in obs$timestep) {
-    if (progress) pb$tick()
+  for (t in .obs$timestep) {
+    if (.progress) pb$tick()
     cat_to_cf(paste0("... Time step ", t, ":"))
 
     #### Define location given data (detection/non detection)
-    if (obs$detection[t] == 1) {
+    if (.obs$detection[t] == 1) {
       # (1) Define location _given_detection_ at current time step
-      cat_to_cf(paste0("... ... Detection ", obs$detection_id[t], ":"))
+      cat_to_cf(paste0("... ... Detection ", .obs$detection_id[t], ":"))
       cat_to_cf(paste0("... ... * Defining location given data..."))
       if (t == 1) {
         # * This is only necessary at the very first time step
@@ -263,31 +264,32 @@
         # * ... the kernel around the next receiver(s) (see below)
 
         # Identify receiver(s) that recorded detections at the selected time step
-        detections_current <- obs$receiver_id[t][[1]]
+        detections_current <- .obs$receiver_id[t][[1]]
         # Identify remaining (active) receivers which did not record a detection (if any)
-        absences_current <- .acs_absences(obs$date[t], detections_current, detection_overlaps)
-        given_data       <- .acs_given_detection(detections_current, absences_current, detection_kernels)
+        absences_current <- .acs_absences(.obs$date[t], detections_current, .detection_overlaps)
+        given_data       <- .acs_given_detection(detections_current, absences_current, .detection_kernels)
       }
     } else {
       # (2) Define location _given non-detection_ at current time step
-      given_data <- detection_kernels$bkg_inv_surface_by_design[[detection_kernels$array_design_by_date[[obs$date[t]]]]]
+      given_data <- .detection_kernels$bkg_inv_surface_by_design[[.detection_kernels$array_design_by_date[[.obs$date[t]]]]]
     }
-    # Filter given_data by depth
-    # TO DO
-    # * use calc_depth_error()
+    # Filter by depth (andor other constraints)
+    if (!is.null(.update_ac)) {
+      given_data <- .update_ac(given_data, .bathy, .obs, t, ...)
+    }
 
     #### Define location given past
     if (t > 1) {
       cat_to_cf(paste0("... ... * Defining location given past..."))
-      given_past <- terra::buffer(terra::classify(past, cbind(0, NA)), obs$buffer_past[t])
+      given_past <- terra::buffer(terra::classify(past, cbind(0, NA)), .obs$buffer_past[t])
     } else {
       given_past <- NULL
     }
 
     #### Define location given future
-    if (obs$detection_id[t] < detection_id_final) {
+    if (.obs$detection_id[t] < detection_id_final) {
       cat_to_cf(paste0("... ... * Defining location given future..."))
-      if (obs$detection[t] == 1) {
+      if (.obs$detection[t] == 1) {
         # (1) Define the location at the next detection
         # * We only need to do this once per acoustic time step & if
         # ... a) the receivers that recorded the detection or
@@ -295,18 +297,18 @@
         # * Otherwise, we automatically reuse next_kernel from the previous
         # ... acoustic time step
         cat_to_cf(paste0("... ... - Defining future kernel..."))
-        t_next <- obs$timestep[obs$detection_id == obs$detection_id[t] + 1][1]
-        detections_next <- obs$receiver_id[t_next][[1]]
-        absences_next   <- .acs_absences(obs$date[t_next], detections_next, detection_overlaps)
+        t_next <- .obs$timestep[.obs$detection_id == .obs$detection_id[t] + 1][1]
+        detections_next <- .obs$receiver_id[t_next][[1]]
+        absences_next   <- .acs_absences(.obs$date[t_next], detections_next, .detection_overlaps)
         if (t == 1 | !isTRUE(all.equal(detections_current, detections_next)) | !isTRUE(all.equal(absences_current, absences_next))) {
-          next_kernel <- .acs_given_detection(detections_next, absences_next, detection_kernels, .zero_to_na = TRUE)
+          next_kernel <- .acs_given_detection(detections_next, absences_next, .detection_kernels, .zero_to_na = TRUE)
         }
       }
       # (2) Define given_future for current time step
       # * Note that `next_kernel` has already been correctly specified
       # * ... with zeros set to NA to ensure correct buffering
       cat_to_cf(paste0("... ... - Defining future buffer..."))
-      given_future <- terra::buffer(next_kernel, obs$buffer_future[t])
+      given_future <- terra::buffer(next_kernel, .obs$buffer_future[t])
     } else {
       given_future <- NULL
     }
@@ -328,10 +330,10 @@
     if (!is.null(given_future)) {
       present <- terra::mask(present, given_future, maskvalues = 0, updatevalue = 0)
     }
-    present <- terra::mask(present, bathy)
+    present <- terra::mask(present, .bathy)
     # Renormalise
-    present <- present / as.numeric(terra::global(present, "sum", na.rm = TRUE))
-    if (prompt) {
+    present <- normalise(present)
+    if (.prompt) {
       terra::plot(present, main = glue::glue("t = {t}"))
       given_data |>
         terra::as.polygons() |>
@@ -352,21 +354,21 @@
     #### Update record
     cat_to_cf(paste0("... ... * Updating record ..."))
     # Save 'present' in memory
-    if (save_record) {
+    if (.save_record) {
       record[[t]] <- present
     }
     # Write 'present' to file
-    if (!is.null(write_record)) {
-      write_record$x <- present
-      write_record$filename <- file.path(write_record_folder, paste0(obs$timestep[t], ".tif"))
-      do.call(terra::writeRaster, write_record)
+    if (!is.null(.write_record)) {
+      .write_record$x <- present
+      .write_record$filename <- file.path(write_record_folder, paste0(.obs$timestep[t], ".tif"))
+      do.call(terra::writeRaster, .write_record)
     }
     # Update cumulative map
     #
     # TO DO
     # * Confirm with SB about adding normalised surfaces
     #
-    if (save_cumulative) {
+    if (.save_cumulative) {
       if (t == 1) {
         cumulative <- present
       } else {
@@ -379,7 +381,7 @@
     # The present becomes the past (at the next time step)
     past <- present
     # When we change acoustic time steps, the next kernel becomes the current one
-    if (obs$detection_id[t] < detection_id_final && obs$detection_id[t] != obs$detection_id[t + 1]) {
+    if (.obs$detection_id[t] < detection_id_final && .obs$detection_id[t] != .obs$detection_id[t + 1]) {
       given_data          <- next_kernel
       detections_current  <- detections_next
       absences_current    <- absences_next
@@ -388,8 +390,8 @@
   }
 
   #### Renormalise cumulative map
-  if (save_cumulative) {
-    cumulative <- cumulative / as.numeric(terra::global(cumulative, "sum", na.rm = TRUE))
+  if (.save_cumulative) {
+    cumulative <- normalise(cumulative)
   }
 
   #### Define output list
