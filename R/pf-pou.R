5c929172c6283a2868707095bd282168d96172b0
edwardlavender <el72@st-andrews.ac.uk>
2023-09-25 17:58:18 +0200

    Add pf_pou() function

---
+++ R/pf-pou.R
@@ -0,0 +1,56 @@
+#' @title PF: Map probability-of-use
+#' @description This function builds a 'probability-of-use' utilisation distribution from (processed) particle samples.
+#' @param .history The (processed) particle samples, provided as:
+#' * A `list` of [`data.table`]s that define cell samples; i.e., the `history` element of a [`pf-class`] object. This must contain a column that defines cell samples at each time step named `cell_now`.
+#' * A `character` string that defines the directory in which particle samples were written (as parquet files).
+#' @param .bathy A [`SpatRaster`] that defines the grid for the utilisation distribution. `NAs` on `.bathy` are used as a mask.
+#' @param .plot A logical input that defines whether or not to plot the [`SpatRaster`].
+#' @param ... If `.plot = TRUE`, `...` is a place holder for additional arguments passed to [`terra::plot()`].
+#' @details Probability-of-use is the proportion of samples of each unique cell (out of the total number of samples across all time steps).
+#' @return The function returns a [`SpatRaster`] (utilisation distribution) in which cell values define probability-of-use.
+#' @example man/examples/pf-pou-examples.R
+#' @author Edward Lavender
+#' @export
+
+pf_pou <-
+  function(.history, .bathy, .plot = TRUE, ...) {
+
+    #### Check user inputs
+    check_inherits(.history, c("character", "list"))
+
+    #### Tabulate cell frequencies
+    # Prepare samples from parquet files
+    if (inherits(.history, "character")) {
+      check_dir(.history)
+      if (!all((list.files(.history) |> tools::file_ext()) == "parquet")) {
+        abort("`.history` contains non parquet files.")
+      }
+      samples <-
+        arrow::open_dataset(.history, format = "parquet") |>
+        arrow::to_duckdb()
+    } else {
+      # Prepare samples from pf_*() list
+      check_names(.history[[1]], req = "cell_now")
+      samples <- .history |> rbindlist()
+    }
+    # Calculate POU from samples
+    pou <-
+      samples |>
+      dplyr::select(cell = "cell_now") |>
+      dplyr::count(.data$cell) |>
+      dplyr::collect() |>
+      dplyr::mutate(pr = .data$n / sum(.data$n))
+
+    #### Build SpatRaster
+    map <- terra::setValues(.bathy, 0)
+    map <- terra::mask(map, .bathy)
+    map[pou$cell] <- pou$pr
+    if (.plot) {
+      terra::plot(map, ...)
+    }
+
+    #### Return outputs
+    map
+
+}
+
---
+++ man/examples/pf-pou-examples.R
@@ -0,0 +1,57 @@
+#### Set up examples
+
+# (A) Define input datasets
+acoustics <- dat_acoustics[individual_id == 25, ]
+archival <- dat_archival[individual_id == 25, ]
+obs <- acs_setup_obs(acoustics, archival, "2 mins", 500)
+obs <- obs[1:200, ]
+gebco <- dat_gebco()
+
+# (B) Implement AC* algorithm
+dat_moorings$receiver_range <- 500
+containers <- acs_setup_detection_containers(gebco, dat_moorings)
+overlaps <- acs_setup_detection_overlaps(containers, dat_moorings)
+kernels <-
+  acs_setup_detection_kernels(dat_moorings,
+                              .calc_detection_pr = acs_setup_detection_pr,
+                              .bathy = gebco)
+ac_folder <- file.path(tempdir(), "ac")
+dir.create(ac_folder)
+out_ac <-
+  acs(obs,
+      .bathy = gebco,
+      .detection_overlaps = overlaps,
+      .detection_kernels = kernels,
+      .save_record = TRUE)
+
+# (C) Implement forward simulation
+forward_folder <- file.path(tempdir(), "pf", "forward")
+dir.create(forward_folder, recursive = TRUE)
+out_pf <- pf_forward(.obs = obs,
+                     .record = out_ac$record,
+                     .n = 1e3,
+                     .kick = pf_setup_kick,
+                     .bathy = gebco,
+                     .save_history = TRUE,
+                     .write_history = list(sink = forward_folder))
+
+# (D) Implement backward pass
+# * TO DO
+
+#### Example (1): Calculate POU from pf object
+pou_1 <- pf_pou(.history = out_pf$history, .bathy = gebco)
+
+#### Example (2): Calculate POU from parquet files
+pou_2 <- pf_pou(.history = forward_folder, .bathy = gebco)
+stopifnot(terra::all.equal(pou_1, pou_2))
+
+#### Example (3): Customise plot via ...
+pou <- pf_pou(.history = forward_folder,
+              .bathy = gebco,
+              col = grDevices::cm.colors(100))
+get_hr_full(pou, .add = TRUE, lwd = 0.25)
+get_hr_core(pou, .add = TRUE, lwd = 0.75)
+
+# Clean up
+unlink(ac_folder, recursive = TRUE)
+unlink(forward_folder, recursive = TRUE)
