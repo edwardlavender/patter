a5fa52da8be7cd027375ed5ad0e59de06e2c2eef
edwardlavender <el72@st-andrews.ac.uk>
2023-09-25 21:44:07 +0200

    Add initial implementation of pf_backward()

---
+++ R/pf_backward.R
@@ -0,0 +1,117 @@
+#' @title PF: backward simulation
+#' @description This function implements the backward correction of particle samples.
+#' @param .history Particle samples from the forward simulation, provided either as:
+#' * A `list` of [`data.table`]s that define cell samples; i.e., the `history` element of a [`pf-class`] object. This must contain columns that define cell samples at each time step (`cell_now`) alongside previous samples (`cell_past`).
+#' * An ordered list of file paths (from [`pf_setup_record()`]) that define the directories in which particle samples were written from the forward simulation (as parquet files).
+#' @param .save_history A logical variable that defines whether or not to save updated particle samples in memory (see [`pf_forward()`]).
+#' @param .write_history A named list, passed to [`arrow::write_parquet()`], to write updated particle samples to file (see [`pf_forward()`]).
+#' @param .progress,.verbose,.con Arguments to monitor function progress (see [`pf_forward()`]).
+#'
+#' @details At the time of writing, this function only removes 'dead ends' from particle samples. Backwards smoothing is not currently implemented.
+#'
+#' @example man/examples/pf_backward-examples.R
+#'
+#' @return The function returns a [`pf-class`] object.
+#'
+#' @seealso [`pf_forward()`] implements the forward simulation.
+#' @author Edward Lavender
+#' @export
+
+pf_backward <- function(.history, .save_history = FALSE, .write_history = NULL,
+                        .progress = TRUE, .verbose = TRUE, .con = "") {
+
+  #### Check user inputs
+  t_onset <- Sys.time()
+  check_inherits(.history, "list")
+  if (!.save_history && is.null(.write_history)) {
+    abort("`.save_history = FALSE` and `.write_history = NULL`. There is nothing to do.")
+  }
+  write_history_folder <- .pf_check_write_history(.write_history)
+  if (!.verbose & .con != "") {
+    warn("Input to `.con` ignored since `.verbose = FALSE`.")
+  }
+
+  #### Set up messages (modied from pf_forward())
+  if (.verbose && .con != "") {
+    create_log(.con)
+  }
+  append_messages <- ifelse(.con == "", FALSE, TRUE)
+  cat_to_cf <- function(..., message = .verbose, file = .con, append = append_messages) {
+    if (message) cat(paste(..., "\n"), file = .con, append = append)
+  }
+  cat_to_cf(paste0("patter::pf_backward() called (@ ", t_onset, ")..."))
+  on.exit(cat_to_cf(paste0("patter::pf_backward() call ended (@ ", Sys.time(), ").")), add = TRUE)
+
+  #### Set up loop
+  # Define whether or not .history dataframes need to be read from file
+  if (inherits(.history[[1]], "data.frame")) {
+    read_history <- FALSE
+  } else {
+    read_history <- TRUE
+  }
+  timestep_final <- length(.history)
+  # Define progress bar
+  if (.progress) {
+    pb <- progress::progress_bar$new(total = timestep_final)
+    pb$tick(0)
+  }
+
+  #### Implement loop
+  for (t in rev(seq_len(timestep_final))) {
+
+    #### Read particle samples for t and t - 1
+    if (.progress) pb$tick()
+    cat_to_cf(paste0("... Time step ", t, ":"))
+    if (read_history) {
+      if (t == timestep_final) {
+        cat_to_cf(paste0("... ... Reading `.history[[", timestep_final, "]]`..."))
+        .history[[t]] <- arrow::read_parquet(.history[[t]])
+      }
+      if (t > 1) {
+        cat_to_cf(paste0("... ... Reading `.history[[", t - 1, "]]`..."))
+        .history[[t - 1]] <- arrow::read_parquet(.history[[t - 1]])
+      }
+    }
+
+    #### Filter particle samples
+    # cell_past for current time step should match cell_now for previous one
+    if (t < timestep_final && t > 1) {
+      cat_to_cf(paste0("... ... Cleaning `.history[[", t - 1, "]]`..."))
+      cat_to_cf(paste0("... ... Identifying `cell_now` (for the previous step) that match `cell_past` (for the current step)..."))
+      cat_to_cf(paste0("... ... ... Input: ", nrow(.history[[t]]), " rows in `.history[[t]]`..."))
+      bool <- .history[[t - 1]]$cell_now %in% .history[[t]]$cell_past
+      if (!all(bool)) {
+        cat_to_cf(paste0("... ... ... Filtering ", length(which(!bool)), " dead ends (", length(which(bool)), " remain)..."))
+        .history[[t - 1]] <- .history[[t - 1]] |> filter(bool) |> as.data.table()
+        cat_to_cf(paste0("... ... ... Output: ", nrow(.history[[t - 1]]), " rows in `.history[[t - 1]]`..."))
+      }
+    }
+
+    #### Save particles
+    # Write particles to file
+    cat_to_cf(paste0("... ... Recording (cleaned) outputs for `.history[[", t, "]]`..."))
+    if (!is.null(.write_history)) {
+      .write_history$x    <- .history[[t]]
+      .write_history$sink <- file.path(write_history_folder, paste0(t, ".parquet"))
+      do.call(arrow::write_parquet, .write_history)
+    }
+    # Drop saved particles for current time step, if necessary
+    if (is.null(.save_history)) {
+      .history[[t]] <- NA
+    }
+
+  }
+
+  #### Return outputs (modified from pf_forward())
+  t_end <- Sys.time()
+  time <- list(start = t_onset,
+               end = t_onset,
+               duration = difftime(t_end, t_onset))
+  out <- list(history = .history,
+              time = time)
+
+  #### Return outputs
+  class(out) <- c(class(out), "pf")
+  out
+
+}
---
+++ man/examples/pf_backward-examples.R
@@ -0,0 +1,66 @@
+#### Set up examples
+
+# (A) Define input datasets
+acoustics <- dat_acoustics[individual_id == 25, ]
+archival <- dat_archival[individual_id == 25, ]
+obs <- acs_setup_obs(acoustics, archival, "2 mins", 500)
+obs <- obs[1:200, ]
+gebco <- dat_gebco()
+
+# (B) Implement AC* algorithm
+dat_moorings$receiver_range <- 500
+containers <- acs_setup_detection_containers(gebco, dat_moorings)
+overlaps <- acs_setup_detection_overlaps(containers, dat_moorings)
+kernels <-
+  acs_setup_detection_kernels(dat_moorings,
+                              .calc_detection_pr = acs_setup_detection_pr,
+                              .bathy = gebco)
+ac_folder <- file.path(tempdir(), "ac")
+dir.create(ac_folder)
+out_ac <-
+  acs(obs,
+      .bathy = gebco,
+      .detection_overlaps = overlaps,
+      .detection_kernels = kernels,
+      .save_record = TRUE)
+
+# (C) Implement forward simulation
+forward_folder <- file.path(tempdir(), "pf", "forward")
+dir.create(forward_folder, recursive = TRUE)
+out_pff <- pf_forward(.obs = obs,
+                      .record = out_ac$record,
+                      .n = 1e3,
+                      .kick = pf_setup_kick,
+                      .bathy = gebco,
+                      .save_history = TRUE,
+                      .write_history = list(sink = forward_folder))
+
+#### Example (1): Implement backward simulation from `pf` object
+out_pfb <- pf_backward(out_pff$history, .save_history = TRUE)
+
+#### Example (2): Implement backward simulation from parquet files
+out_pfb <- pf_backward(pf_setup_record(forward_folder),
+                       .save_history = TRUE)
+
+#### Example (3): Write history to file (as in `pf_forward()`)
+backward_folder <- file.path(tempdir(), "pf", "backward")
+dir.create(backward_folder, recursive = TRUE)
+out_pfb <- pf_backward(pf_setup_record(forward_folder),
+                       .write_history = list(sink = backward_folder))
+utils::head(list.files(backward_folder))
+
+#### Example (4): Control monitoring of function progress
+# Suppress progress bar
+out_pfb <- pf_backward(pf_setup_record(forward_folder),
+                       .save_history = TRUE,
+                       .progress = FALSE)
+# Save messages
+log.txt <- tempfile(fileext = ".txt")
+out_pfb <- pf_backward(pf_setup_record(forward_folder),
+                       .save_history = TRUE,
+                       .con = log.txt)
+utils::head(readLines(log.txt), 15)
+# Suppress messages
+out_pfb <- pf_backward(pf_setup_record(forward_folder),
+                       .save_history = TRUE,
+                       .verbose = FALSE)
