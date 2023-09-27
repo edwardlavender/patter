85243aeb3ff4b6add6be362d2a8be1205c499f43
edwardlavender <el72@st-andrews.ac.uk>
2023-09-27 22:48:46 +0200

    Update tests

--- tests/testthat/test-pf-internals.R
+++ tests/testthat/test-pf-internals.R
@@ -16,6 +16,7 @@ test_that(".pf_check_*() functions work", {
 test_that(".pf_path_join() works", {
 
   # Define example data tables to join
+  pb <- progress::progress_bar$new(total = 1)
   d1 <- data.table(x0 = NA, x1 = 1:5)
   d2 <- data.table(cell_past = c(1, 2, 2, 3, 4),
                    cell_now = c(2, 6, 7, 8, 8))
@@ -23,7 +24,7 @@ test_that(".pf_path_join() works", {
                    cell_now = c(10, 11, 12, 13, 13))
 
   # Test joining d1 and d2
-  j1a <- .pf_path_join(d1, d2, .t = 2)
+  j1a <- .pf_path_join(d1, d2, .t = 2, .pb = pb)
   j2a <-
     dplyr::right_join(d1, d2, by = c("x1" = "cell_past")) |>
     select(x0, x1, x2 = cell_now)
@@ -42,6 +43,10 @@ test_that(".pf_path_join() works", {
 })
 
 test_that(".pf_path_chain() works", {
+
+  # Test checks
+  .pf_path_chain(as.list(1:2)) |>
+    expect_error("There are <= 2 steps in the time series.")
 
   # Test with .read = TRUE
   chain_1 <- .pf_path_chain(as.list(1:5), .read = TRUE)
