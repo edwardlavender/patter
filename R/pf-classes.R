474f8075260be9efbcf304c6c46e5f23d07f9f66
edwardlavender <el72@st-andrews.ac.uk>
2023-09-27 22:49:29 +0200

    pf_*() returns a pf-class object

---
+++ R/pf-classes.R
@@ -0,0 +1,7 @@
+#' @title PF outputs: [`pf-class`] objects
+#' @description An S3 class that defines the named `list` returned by a PF-branch algorithm.
+#'
+#' @author Edward Lavender
+#' @docType package
+#' @name pf-class
+NULL
