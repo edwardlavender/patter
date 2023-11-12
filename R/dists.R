ebf1c40ae030998ab5cb3bbdccb034c30deb43fb
edwardlavender <el72@st-andrews.ac.uk>
2023-11-12 12:10:56 +0100

    Add dists_btw_cells() function

--- R/dists.R
+++ R/dists.R
@@ -1,4 +1,4 @@
-#' @title Calculate serial distances
+#' @title Distances: along a path
 #' @description This function calculates distances between sequential coordinates in a matrix.
 #' @param .xy A two-column matrix of coordinates.
 #' @param .lonlat A `logical` variable that defines whether or not the coordinates are in longitude/latitude format.
@@ -9,4 +9,20 @@ dist_along_path <- function(.xy, .lonlat = FALSE) {
   terra::distance(as.matrix(.xy, ncol = 2),
                   lonlat = .lonlat,
                   sequential = TRUE)
+}
+
+#' @title Distances: between cells on a [`SpatRaster`]
+#' @description This function calculates distances between cells on a [`SpatRaster`]
+#' @param .bathy A [`SpatRaster`].
+#' @param .from,.to Integer vectors that define cell IDs.
+#' @param .lonlat A `logical` variable that defines whether or not the [`.bathy`] is in longitude/latitude format.
+#' @param ... Arguments passed to  [`terra::distance()`]
+#' @export
+
+dist_btw_cells <- function(.bathy,
+                           .from, .to,
+                           .lonlat = FALSE, ...) {
+  from <- terra::xyFromCell(.bathy, .from)
+  to   <- terra::xyFromCell(.bathy, .to)
+  terra::distance(from, to, .lonlat, ...)
 }
