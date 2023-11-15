5eaf27ffa4ed642a31f25ad10891826326a6644d
edwardlavender <el72@st-andrews.ac.uk>
2023-11-15 13:26:56 +0100

    Fix tests for .coa_check_lonlat() (now .is_lonlat())

--- tests/testthat/test-coa-internals.R
+++ tests/testthat/test-coa-internals.R
@@ -17,37 +17,4 @@ test_that(".coa_check_*() functions works", {
   .coa_check_acoustics(dat_acoustics, .split = "blah") |>
     expect_error("Argument '.acoustics' does not contain all required names. One or more of the following name(s) are missing: 'blah'.", fixed = TRUE)
 
-  #### Test .coa_check_lonlat()
-  # Check TRUE/FALSE
-  .coa_check_lonlat(data.table(receiver_easting = 0, receiver_northing = 0)) |>
-    expect_false()
-  .coa_check_lonlat(data.table(receiver_easting = 0, receiver_northing = 0,
-                               receiver_lon = 0, receiver_lat = 0)) |>
-    expect_false() |>
-    expect_warning("UTM coordinates used (both UTM and lon/lat coordinates detected).",
-                   fixed = TRUE)
-  .coa_check_lonlat(data.table(receiver_lon = 0, receiver_lat = 0)) |>
-    expect_true()
-  # Check for errors if not all coordinate columns are provided
-  lapply(c("blah",
-           "receiver_easting", "receiver_northing",
-           "receiver_lon", "receiver_lat"), function(x) {
-
-             d <- data.table(x = 0)
-             colnames(d) <- x
-             .coa_check_lonlat(d) |>
-               expect_error("Neither UTM coordinates (`.acoustics$receiver_easting`, `.acoustics$receiver_northing`) nor lon/lat coordinates (`.acoustics$receiver_lon`, `.acoustics$receiver_lat`) detected.",
-                            fixed = TRUE)
-
-           }) |> invisible()
-  # Check for errors in lon/lat coordinates
-  .coa_check_lonlat(data.table(receiver_lon = -181, receiver_lat = 0)) |>
-    expect_error("Longitudes should be between -180 and 180 degrees.", fixed = TRUE)
-  .coa_check_lonlat(data.table(receiver_lon = 181, receiver_lat = 0)) |>
-    expect_error("Longitudes should be between -180 and 180 degrees.", fixed = TRUE)
-  .coa_check_lonlat(data.table(receiver_lon = 0, receiver_lat = -91)) |>
-    expect_error("Latitudes should be between -90 and 90 degrees.", fixed = TRUE)
-  .coa_check_lonlat(data.table(receiver_lon = 0, receiver_lat = 91)) |>
-    expect_error("Latitudes should be between -90 and 90 degrees.", fixed = TRUE)
-
 })
--- tests/testthat/test-helpers-spatial.R
+++ tests/testthat/test-helpers-spatial.R
@@ -1,3 +1,40 @@
+test_that(".is_lonlat() works", {
+
+  # Check TRUE/FALSE
+  .is_lonlat(data.table(receiver_easting = 0, receiver_northing = 0)) |>
+    expect_false()
+  .is_lonlat(data.table(receiver_easting = 0, receiver_northing = 0,
+                        receiver_lon = 0, receiver_lat = 0)) |>
+    expect_false() |>
+    expect_warning("UTM coordinates used (both UTM and lon/lat coordinates detected).",
+                   fixed = TRUE)
+  .is_lonlat(data.table(receiver_lon = 0, receiver_lat = 0)) |>
+    expect_true()
+
+  # Check for errors if not all coordinate columns are provided
+  lapply(c("blah",
+           "receiver_easting", "receiver_northing",
+           "receiver_lon", "receiver_lat"), function(x) {
+             d <- data.table(x = 0)
+             colnames(d) <- x
+             .is_lonlat(d) |>
+               expect_error("Neither UTM coordinates (`.data$receiver_easting`, `.data$receiver_northing`) nor lon/lat coordinates (`.data$receiver_lon`, `.data$receiver_lat`) detected.",
+                            fixed = TRUE)
+
+           }) |> invisible()
+
+  # Check for errors in lon/lat coordinates
+  .is_lonlat(data.table(receiver_lon = -181, receiver_lat = 0)) |>
+    expect_error("Longitudes should be between -180 and 180 degrees.", fixed = TRUE)
+  .is_lonlat(data.table(receiver_lon = 181, receiver_lat = 0)) |>
+    expect_error("Longitudes should be between -180 and 180 degrees.", fixed = TRUE)
+  .is_lonlat(data.table(receiver_lon = 0, receiver_lat = -91)) |>
+    expect_error("Latitudes should be between -90 and 90 degrees.", fixed = TRUE)
+  .is_lonlat(data.table(receiver_lon = 0, receiver_lat = 91)) |>
+    expect_error("Latitudes should be between -90 and 90 degrees.", fixed = TRUE)
+
+})
+
 test_that("rast_template() works", {
   # Test default options
   r <- rast_template()
