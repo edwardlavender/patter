4856a13bb44ed7ccc16fb38be0fa9bbd4d653f10
edwardlavender <el72@st-andrews.ac.uk>
2023-11-21 10:11:41 +0000

    Reorganise pf_path_*() and pf_map_*() functions

---
+++ R/pf_path-internals.R
@@ -0,0 +1,48 @@
+#' @title PF: path reconstruction helpers
+#' @description These functions facilitate the reconstruction of movement paths.
+#' @param .data,.current.,t.,pb Arguments for `.pf_path_join`.
+#' @param .history,.read Arguments for `.pf_path_chain`.
+#' @author Edward Lavender
+#' @name pf_path_
+
+#' @rdname pf_path_
+#' @keywords internal
+
+.pf_path_join <-
+  function(.data, .current, .t, .pb = NULL) {
+    # Monitor progress
+    # print(.t)
+    # Use Sys.sleep() for testing (e.g., visualise progress bar)
+    # Sys.sleep(0.1)
+    if (!is.null(.pb)) {
+      .pb$tick()
+    }
+    collapse::join(.data,
+                   .current |>
+                     select("x{.t - 1}" := "cell_past", "x{.t}" := "cell_now") |>
+                     as.data.table(),
+                   how = "right",
+                   validate = "m:m",
+                   on = paste0("x", .t - 1),
+                   verbose = FALSE)
+  }
+
+#' @rdname pf_path_
+#' @keywords internal
+
+.pf_path_chain <-
+  function(.history, .read){
+    if (length(.history) <= 2L) {
+      abort("There are <= 2 steps in the time series.")
+    }
+    index <- 2:length(.history)
+    if (.read) {
+      history_for_index <- paste0("arrow::read_parquet(.history[[", index, "]])")
+    } else {
+      history_for_index <- paste0(".history[[", index, "]]")
+    }
+    paste0(
+      ".history[[1]] |> \n",
+      paste(paste0("  .pf_path_join(", history_for_index, ", .t = ", index, ", .pb = .pb)"), collapse = " |> \n")
+    )
+  }
