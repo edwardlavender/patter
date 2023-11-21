b9f8c9e07e8db0152369fb873968737d44149e5d
edwardlavender <el72@st-andrews.ac.uk>
2023-11-21 09:41:14 +0000

    Add seealso to dist_*() functions

--- R/dists.R
+++ R/dists.R
@@ -3,6 +3,10 @@
 #' @param .xy A two-column matrix of coordinates.
 #' @param .lonlat A `logical` variable that defines whether or not the coordinates are in longitude/latitude format.
 #' @details This function is a simple wrapper for [`terra::distance()`].
+#' @seealso
+#' * `dist_*` functions in [`flapper`](https://github.com/edwardlavender/flapper);
+#' * [`dist_along_path()`] calculates distances along a path;
+#' * [`dist_btw_cells()`] calculates distances between [`SpatRaster`] cells;
 #' @export
 
 dist_along_path <- function(.xy, .lonlat = FALSE) {
@@ -16,7 +20,11 @@ dist_along_path <- function(.xy, .lonlat = FALSE) {
 #' @param .bathy A [`SpatRaster`].
 #' @param .from,.to Integer vectors that define cell IDs.
 #' @param .lonlat A `logical` variable that defines whether or not the `.bathy` is in longitude/latitude format.
-#' @param ... Arguments passed to  [`terra::distance()`]
+#' @param ... Arguments passed to  [`terra::distance()`].
+#' @seealso
+#' * `dist_*` functions in [`flapper`](https://github.com/edwardlavender/flapper);
+#' * [`dist_along_path()`] calculates distances along a path;
+#' * [`dist_btw_cells()`] calculates distances between [`SpatRaster`] cells;
 #' @export
 
 dist_btw_cells <- function(.bathy,
