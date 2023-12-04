3c92d901d3ce180ea4854394adb09eaa3fd288ae
edwardlavender <el72@st-andrews.ac.uk>
2023-12-04 12:45:29 +0000

    Fix dist_along_path() test

--- tests/testthat/test-dists.R
+++ tests/testthat/test-dists.R
@@ -2,6 +2,6 @@ test_that("dist_along_path() works", {
   xy <- cbind(c(1, 2, 3), c(3, 2, 1))
   expect_equal(
     dist_along_path(xy, .lonlat = FALSE),
-    terra::distance(xy, lonlat = FALSE, sequential = TRUE)
+    c(terra::distance(xy, lonlat = FALSE, sequential = TRUE)[2:nrow(xy)], NA_real_)
   )
 })
