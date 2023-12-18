9c73c4aaa38be31e90c28b819b5436c6284b14c8
edwardlavender <el72@st-andrews.ac.uk>
2023-12-18 13:55:42 +0000

    Keep dat_obs()

--- R/dats-patter.R
+++ R/dats-patter.R
@@ -3,11 +3,13 @@
 #' @description These functions load example outputs from key [`patter`] functions. They are included in the package to streamline function examples and tests.
 #'
 #' @details
+#' * [`dat_pff()`] reads an example output from [`acs_setup_obs()`];
 #' * [`dat_pff()`] reads an example output from [`pf_forward()`];
 #' * [`dat_pfbk()`] reads an example output from [`pf_backward_killer()`];
 #' * [`dat_pfp()`] reads an example output from [`pf_path()`];
 #'
 #' @examples
+#' dat_obs() |> summary()
 #' dat_pff() |> summary()
 #' dat_pfbk() |> summary()
 #' dat_pfp() |> summary()
@@ -20,26 +22,35 @@
 #' @rdname  datasets-algorithms
 #' @export
 
-dat_pff <- function() {
-  path <- system.file("extdata", "dat_pff.rds",
+dat_obs <- function() {
+  data <- system.file("extdata", "dat_obs.rds",
                       package = "patter", mustWork = TRUE)
-  readRDS(path)
+  readRDS(data)
+}
+
+#' @rdname  datasets-algorithms
+#' @export
+
+dat_pff <- function() {
+  data <- system.file("extdata", "dat_pff.rds",
+                      package = "patter", mustWork = TRUE)
+  readRDS(data)
 }
 
 #' @rdname datasets-algorithms
 #' @export
 
 dat_pfbk <- function() {
-  path <- system.file("extdata", "dat_pfbk.rds",
+  data <- system.file("extdata", "dat_pfbk.rds",
                       package = "patter", mustWork = TRUE)
-  readRDS(path)
+  readRDS(data)
 }
 
 #' @rdname datasets-algorithms
 #' @export
 
 dat_pfp <- function() {
-  path <- system.file("extdata", "dat_pfp.rds",
+  data <- system.file("extdata", "dat_pfp.rds",
                       package = "patter", mustWork = TRUE)
-  readRDS(path)
+  readRDS(data)
 }
--- tests/testthat/test-dats-patter.R
+++ tests/testthat/test-dats-patter.R
@@ -3,6 +3,9 @@ test_that("Algorithm datasets can be loaded", {
   # Activate tests
   expect_true(1 == 1)
 
+  x <- dat_obs()
+  x |> check_inherits("data.table")
+
   x <- dat_pff()
   x |> check_inherits("list")
 
