6f90b5d4d86ffbeb5a7e619e0cb2af672154e089
edwardlavender <el72@st-andrews.ac.uk>
2023-12-18 14:54:22 +0000

    Improve .pf_history_dt() documentation

--- R/pf-internal.R
+++ R/pf-internal.R
@@ -1,10 +1,48 @@
-#' @title PF: Compile particle histories into a [`data.table`]
-#' @description This function compiles particle histories into a [`data.table`].
+#' @title PF: collate particle histories
+#' @description This function collates particle histories into a single [`data.table`].
 #' @param .history The particle samples, supplied as:
-#' * A [`pf-class`] object;
-#' * The `history` element of a [`pf-class`] object;
-#' * A string that defines the directory containing parquet files;
-#' * A list of strings that defines parquet files;
+#' * A [`pf_particles-class`] object;
+#' * The `history` element of a [`pf_particles-class`] object;
+#' * A `character` string that defines the directory containing parquet files;
+#' * A `list` of file paths (e.g., from [`pf_setup_files()`]);
+#' @param ... If `.history` is a directory, `...` additional arguments can be passed to [`arrow::open_dataset()`] via `...`.
+#' @param .collect If `.history` is a directory, `.collect` is a `logical` variable that defines whether or not to collect the dataset in memory.
+#'
+#' @details
+#'
+#' 1. If `.history` is a [`pf_particles-class`] object, or the history element of such an object, `data.table::rbindlist(..., fill = TRUE)` is used to combine particle samples.
+#'
+#' 2. If `.history` is a folder, [`arrow::open_dataset()`] plus (optionally) [`dplyr::collect()`] is used. Additional arguments, such as `schema` can be passed to [`arrow::open_dataset()`] via `...`.
+#'
+#' 3. If `.history` is a `list` of parquet files, files are iteratively read into memory (via [`arrow::read_parquet()`]) and combined (via `data.table::rbindlist(..., fill = TRUE)`. This option is much slower than option 2.
+#'
+#' @return The function returns a [`data.table`] or an [`arrow::FileSystemDataset`] (if `.history` is a directory and `.collect = FALSE`).
+#'
+#' @examples
+#' \dontrun{
+#' require(arrow)
+#'
+#' # Use pf_particles-class object
+#' .pf_history_dt(dat_pfbk())
+#'
+#' # Use history element
+#' .pf_history_dt(dat_pfbk()$history)
+#'
+#' # Use directory (and optionally select a subset of columns)
+#' pfbk_folder <- system.file("extdata", "acpf", "backward", "killer",
+#'                            package = "patter", mustWork = TRUE)
+#' sch <- schema(timestep = int32(),
+#'               cell_now = int32(),
+#'               x_now = double(),
+#'               y_now = double())
+#' .pf_history_dt(pfbk_folder, schema = sch)
+#'
+#' # Use directory with `.collect = FALSE`
+#' .pf_history_dt(pfbk_folder, schema = sch, .collect = FALSE)
+#'
+#' # Use a file list
+#' .pf_history_dt(pf_setup_files(pfbk_folder))
+#' }
 #' @author Edward Lavender
 #' @keywords internal
 
@@ -58,5 +96,5 @@
   }
 
   # Return error on failure
-  abort("`.history` should be the path to parquet files or a list of data.tables.")
+  abort("`.history` should be a pf_particles-class object, the `history` element of a pf_particles-class object, a list of file paths or a directory containing history (.parquet) files.")
 }
