58ae427a0f641167f06e3acf87f1caea5d4fb0ff
edwardlavender <el72@st-andrews.ac.uk>
2023-12-18 13:44:29 +0000

    Drop old tests

--- tests/testthat/test-dats-patter.R
+++ tests/testthat/test-dats-patter.R
@@ -3,28 +3,10 @@ test_that("Algorithm datasets can be loaded", {
   # Activate tests
   expect_true(1 == 1)
 
-  # Check algorithm outputs can be loaded
-  # Check SpatRasters have been correctly unwrapped
-  # (* Plot SpatRasters: this will fail if not unwrapped
-
-  x <- dat_obs()
-  x |> check_inherits("data.table")
-
-  x <- dat_overlaps()
-  x |> check_inherits("list")
-
-  x <- dat_kernels()
-  x$array_design |> check_inherits("data.frame")
-  x$receiver_specific_kernels[[4]] |> terra::plot()
-  x$receiver_specific_inv_kernels[[4]] |> terra::plot()
-  x$array_design_by_date |> check_inherits("list")
-  x$bkg_surface_by_design[[1]] |> terra::plot()
-  x$bkg_inv_surface_by_design[[1]] |> terra::plot()
-
   x <- dat_pff()
   x |> check_inherits("list")
 
-  x <- dat_pfb()
+  x <- dat_pfbk()
   x |> check_inherits("list")
 
   x <- dat_pfp()
