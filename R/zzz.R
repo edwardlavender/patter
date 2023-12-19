c58d1387dd19ae0c70ba35b86fa2993f657781ee
edwardlavender <el72@st-andrews.ac.uk>
2023-12-19 12:56:29 +0000

    Add patter.verbose global option

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,21 @@
+.onLoad <- function(libname, pkgname) {
+  # Collect options
+  op <- options()
+  # Define pater options
+  op_patter <- list(patter.verbose = TRUE)
+  # Set necessary patter options
+  bool <- !(names(op_patter) %in% names(op))
+  if (any(bool)) {
+    options(op_patter[bool])
+  }
+  invisible(NULL)
+}
+
+.onUnload <- function(libpath){
+  options("patter.verbose" = NULL)
+  invisible(NULL)
+}
+
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0("This is {patter} v.", utils::packageVersion("patter"), ". For an overview, see `?patter`. For support, contact edward.lavender@eawag.ch."))
 }
