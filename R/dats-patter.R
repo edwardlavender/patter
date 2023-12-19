ed34d2d9c5359e7321b6d74091e83ad69c96e4d9
edwardlavender <el72@st-andrews.ac.uk>
2023-12-19 10:53:29 +0000

    Add dat_coa() function

--- R/dats-patter.R
+++ R/dats-patter.R
@@ -7,12 +7,14 @@
 #' * [`dat_pff()`] reads an example output from [`pf_forward()`];
 #' * [`dat_pfbk()`] reads an example output from [`pf_backward_killer()`];
 #' * [`dat_pfp()`] reads an example output from [`pf_path()`];
+#' * [`dat_coa()`] reads an example output from [`coa()`];
 #'
 #' @examples
 #' dat_obs() |> summary()
 #' dat_pff() |> summary()
 #' dat_pfbk() |> summary()
 #' dat_pfp() |> summary()
+#' dat_coa() |> summary()
 #'
 #' @source For full details on the algorithm parameters used to generate these datasets, see \url{https://github.com/edwardlavender/patter/blob/main/data-raw/002-add-data-algorithms.R}.
 #'
@@ -51,6 +53,15 @@ dat_pfbk <- function() {
 
 dat_pfp <- function() {
   data <- system.file("extdata", "dat_pfp.rds",
+                      package = "patter", mustWork = TRUE)
+  readRDS(data)
+}
+
+#' @rdname datasets-algorithms
+#' @export
+
+dat_coa <- function() {
+  data <- system.file("extdata", "dat_coa.rds",
                       package = "patter", mustWork = TRUE)
   readRDS(data)
 }
