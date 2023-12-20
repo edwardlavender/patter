3ca70f7a97b96e933478d99647e7e840cebd8993
edwardlavender <el72@st-andrews.ac.uk>
2023-12-20 21:26:21 +0000

    Update generic pf_backward_*() docs

--- R/pf_backward.R
+++ R/pf_backward.R
@@ -1,14 +1,17 @@
 #' @title PF: backward pass
 #'
 #' @description
+#' The forward filtering--backward sampling algorithm implemented by [`patter`] that reconstructs animal movements in passive acoustic telemetry systems comprises a forward filter and a backward pass. The forward filter runs a simulation forwards in time, sampling locations (particles) that are consistent with the data up to and including each time point and a movement model (see [`pf_forward()`]). The backward pass refines outputs from the forward filter. [`patter`] provides two options for the backward pass:
 #'
-#' [`patter`] provides two options for the backward simulation:
+#' * [`pf_backward_killer()`] prunes 'dead-ends' from the time series. This function is simple but fast.
+#' * [`pf_backward_sampler()`] implements a backward sampler. This function samples from the joint distribution of particles, given all of the data---both from the past and the future. This is much more expensive.
 #'
-#' * [`pf_backward_killer()`]
-#' * [`pf_backward_sampler()`]
+#' See [`pf_backward_killer_diagnostics()`] to compare particle diagnostics for the two approaches.
 #'
-#' @seealso
+#' @return [`pf_backward_*()`] functions return [`pf_particles-class`] objects.
 #'
+#' @inherit pf_forward seealso
+#' @author Edward Lavender
 #' @name pf_backward_*
 
 NULL
