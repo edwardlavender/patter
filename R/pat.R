4bf62df96992064445f3b0d49ad76dfc78543e77
edwardlavender <el72@st-andrews.ac.uk>
2023-12-20 15:04:14 +0000

    Tweak pf_setup_data() docs

--- R/pat.R
+++ R/pat.R
@@ -1,23 +1,24 @@
 #' @title [`patter`] set up: datasets
-#' @description This function is used to set up movement datasets, spatial datasets and associated parameters for all [`patter`] functions. In general, this function should be called before other [`patter`] functions.
+#' @description This function is used to set up movement datasets, spatial datasets and associated parameters for all [`patter`] functions. In general, [`pat_setup_data()`] should be called before other routines.
 #'
 #' @param .acoustics,.services,.archival,.moorings (optional) Movement datasets.
 #' * `.acoustics` contains the detection time series (see [`dat_acoustics`] for an example);
-#' * `.moorings` contains the receiver deployments (see [`dat_moorings`]) for an example);
+#' * `.moorings` contains receiver deployments (see [`dat_moorings`]) for an example);
 #' * `.services` contains receiver servicing information (times during the deployment period of a receiver when it was not active due to servicing);
 #' * `.archival` contains archival (depth) time series (see [`dat_archival`] for an example);
 #' @param .bathy (optional) A [`SpatRaster`] that defines a (bathymetry) grid over the study area.
-#' @param .lonlat (optional) A `logical` variable that defines whether spatial data is in longitude/latitude format.
+#' @param .lonlat (optional) A `logical` variable that defines whether spatial data (`.moorings` coordinates, `.bathy`) is in longitude/latitude format (`TRUE`) or planar (`FALSE`).
 #'
 #' @details
 #' The preparation of datasets for [`patter`] is a one-off inconvenience. See the [`check_dlist()`] function documentation for full details as to the required properties of each input dataset. All requirements are kept to a minimum and straightforward to address. To minimise inconvenience, all inputs are optional in [`pat_setup_data()`]. If you are only interested in  specific routine(s), check the associated documentation to see which inputs are essential for those routine(s). The output of [`pat_setup_data()`] is widely passed to downstream [`patter`] functions, which streamlines the API, documentation and internal code.
 #'
-#' If `.lonlat = TRUE`, downstream functions require the `geosphere` package.
+#' If `.lonlat = TRUE`, downstream functions may require the `geosphere` package.
 #'
 #' @return The function returns a named `list` with three elements:
-#' * `data`---a named list, with one element for each movement dataset;
-#' * `spatial`---a named list, with one element for each spatial dataset;
-#' * `pars`---a named list, with required parameters;
+#' * `data`---a named `list`, with one element for each movement dataset;
+#' * `spatial`---a named `list`, with one element for each spatial dataset;
+#' * `pars`---a named `list`, with required parameters;
+#' * `algorithm`---an empty `list` that serves as a placeholder for algorithm layers required by [`pf_forward()`] and associated routines;
 #'
 #' @examples
 #' dlist <- pat_setup_data(.acoustics = dat_acoustics,
