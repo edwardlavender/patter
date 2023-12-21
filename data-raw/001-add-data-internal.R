16437e454c479999044a8175abc1597a6ac1bda9
edwardlavender <el72@st-andrews.ac.uk>
2023-12-21 17:38:09 +0000

    Add comments

--- data-raw/001-add-data-internal.R
+++ data-raw/001-add-data-internal.R
@@ -1,2 +1,35 @@
+#########################
+#########################
+#### add-data-internal.R
+
+#### Aims
+# 1) Prepare internal datasets
+
+#### Prerequisites
+# 1) NA
+
+
+#########################
+#########################
+#### Set up
+
+#### Wipe workspace
+rm(list = ls())
+try(pacman::p_unload("all"), silent = TRUE)
+dv::clear()
+
+
+#########################
+#########################
+#### Define internal datasets
+
+#### pf class object name
 pf_class <- "pf_particles"
+
+#### Add datasets
 usethis::use_data(pf_class, internal = TRUE)
+
+
+#### End of code.
+#########################
+#########################
