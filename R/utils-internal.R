5f579237be7a321014778f70c6c655fb9c373288
edwardlavender <el72@st-andrews.ac.uk>
2024-01-12 16:46:31 +0100

    Add back list_merge()

--- R/utils-internal.R
+++ R/utils-internal.R
@@ -134,6 +134,21 @@ cat_init <- function(.verbose) {
 # plyr::compact()
 list_compact <- function(l) l[which(!sapply(l, is.null))]
 
+#' @rdname utils-lists
+#' @keywords internal
+
+# rlist::list.merge() inspired function
+list_merge <- function(...) {
+  lists <- list(...)
+  if (any(vapply(lists, function(x) length(x) > 0L && is.null(names(x)), logical(1L)))) {
+    stop("A named list is expected.", call. = FALSE)
+  }
+  list_modify <- function(x, val) {
+    utils::modifyList(x, val, keep.null = TRUE)
+  }
+  Reduce(list_modify, x = lists, init = list())
+}
+
 #' @title Utilities: column products
 #' @description This function calculates column products for each row in a [`matrix`].
 #' @param .data A [`matrix`].
