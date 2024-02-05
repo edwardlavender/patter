9dffddae6436188159542bfa3cb998abebd9fb82
edwardlavender <el72@st-andrews.ac.uk>
2024-02-05 10:16:58 +0100

    Improve pf_plot_history()

--- R/pf_plot.R
+++ R/pf_plot.R
@@ -6,11 +6,13 @@
 #' @param .forward Particle samples, provided in any format accepted by [`.pf_history_list()`]. Particle samples should be from [`pf_forward()`] if `.backward` is supplied. `cell_now`, `x_now` and `y_now` columns are required.
 #' @param .backward (optional) If .`forward` contains particle samples from the forward run, `.backward` can contain particle samples from the backward pass (e.g., [`pf_backward_killer()`]), provided in any format accepted by [`.pf_history_list()`]. The `cell_now` column is required.
 #' @param .steps `NULL` or an `integer` vector of the time steps for which to map particle samples. `NULL` specifies all time steps.
-#' @param .png (optional) A named `list`, passed to [`grDevices::png()`], to save plots to file. Files are named `{.steps[1]}.png, {.steps[2]}.png, ..., {.steps[N]}.png`.
+#' @param .png (optional) A named `list`, passed to [`grDevices::png()`], to save plots to file. `filename` should be the directory in which to write files. Files are named `{.steps[1]}.png, {.steps[2]}.png, ..., {.steps[N]}.png`. `.png` should be supplied if `.cl` is supplied via `...`.
 #' @param .add_surface,.add_forward Named `list`s for plot customisation.
 #' * `.add_surface` is passed to [`terra::plot()`], excluding `x` and `main`.
 #' * `.add_forward` is passed to [`graphics::points()`], excluding `x` and `y`.
-#' @param .prompt A `logical` variable that defines whether or not to prompt the user for input between plots. This is only used if `.png = NULL` (and there are multiple time steps).
+#' @param .add_layer (optional) A `function` used to add additional layer(s) to the plot, such as `function() {points(x, y)}` where `x` and `y` are (for example) receiver coordinates.
+#' @param .prompt A `logical` variable that defines whether or not to prompt the user for input between plots. This is only used in interactive mode if `.png = NULL` (and there are multiple time steps).
+#' @param ... Additional argument(s) passed to [`cl_lapply()`], such as `.cl`.
 #'
 #' @details For each `.step`, [`terra::plot()`] is used to plot `.dlist$spatial$bathy`. Particle samples in `.forward` are added onto the grid via [`graphics::points()`]. If `.backward` is supplied, particles in `.forward` should be derived from the forward run. Particles in `.forward` but not `.backward` (i.e., dead ends) are shown in red (unless `.add_forward$col` is overridden).
 #'
@@ -29,7 +31,8 @@ pf_plot_history <- function(.dlist,
                             .png = NULL,
                             .add_surface = list(),
                             .add_forward = list(),
-                            .prompt = FALSE
+                            .add_layer = NULL,
+                            .prompt = FALSE, ...
                             ) {
 
   # Check user inputs
@@ -47,26 +50,36 @@ pf_plot_history <- function(.dlist,
     .steps <- seq_len(length(.forward))
   }
 
-  # Define file path
-  sink <- NULL
+  # Define plot layers
+  .add_surface$x <- .dlist$spatial$bathy
+  do_backward    <- !is.null(.backward)
+  do_add_layer   <- !is.null(.add_layer)
+
+  # Define png options
+  sink   <- NULL
+  do_png <- FALSE
   if (!is.null(.png)) {
-    sink <- .png$filename
+    check_named_list(.png)
+    do_png <- TRUE
+    if (!rlang::has_name(.png, "filename")) {
+      .png$filename <- getwd()
+    }
+    sink          <- .png$filename
+    .png_defaults <- list(height = 10, width = 10, units = "in", res = 300)
+    .png          <- list_merge(.png_defaults, .png)
   }
 
   # Loop over time steps and make plots
-  cl_lapply(.steps, .fun = function(t) {
+  cl_lapply(.steps, ..., .fun = function(t) {
 
     # Optionally write image to file
-    if (!is.null(.png)) {
+    if (do_png) {
       .png$filename <- file.path(sink, paste0(t, ".png"))
-      .png_defaults <- list(height = 10, width = 10, units = "in", res = 600)
-      .png          <- list_merge(.png_defaults, .png)
       do.call(grDevices::png, .png)
     }
 
-    # Define surface
+    # Update surface
     .add_surface$main <- paste0("Time ", t)
-    .add_surface$x    <- .dlist$spatial$bathy
 
     # Define particle coordinates for plotting
     fwd <- .pf_history_elm(.history = .forward,
@@ -77,7 +90,7 @@ pf_plot_history <- function(.dlist,
     add_fwd$x <- fwd$x_now
     add_fwd$y <- fwd$y_now
     # Colour dead ends from the forward run in red
-    if (!is.null(.backward)) {
+    if (do_backward) {
       bwd <- .pf_history_elm(.history = .backward,
                              .elm = t,
                              .read = backward_read,
@@ -90,9 +103,12 @@ pf_plot_history <- function(.dlist,
     # Make plot
     do.call(terra::plot, .add_surface)
     do.call(points, add_fwd)
+    if (do_add_layer) {
+      .add_layer()
+    }
 
     # Save image
-    if (!is.null(.png)) {
+    if (do_png) {
       grDevices::dev.off()
     }
 
