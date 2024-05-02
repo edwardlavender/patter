4847f494cd89d1fd1533fff58b0e7112410d7571
edwardlavender <edward.lavender@eawag.ch>
2024-05-02 16:19:17 +0200

    Add helper

--- R/julia-helpers.R
+++ R/julia-helpers.R
@@ -4,3 +4,10 @@ julia_print <- function(x) {
   julia_command("println(x);")
   nothing()
 }
+
+# Format time stamps for julia
+julia_timestamp <- function(x) {
+  check_inherits(x, "POSIXct")
+  x <- check_tz(x)
+  as.POSIXct(format(x, "%Y-%m-%d %H:%M:%S"), tz = lubridate::tz(x))
+}
