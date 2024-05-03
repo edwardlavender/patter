2eda475326dd3c9eeb30c6bb6b6cd8ac07883c61
edwardlavender <edward.lavender@eawag.ch>
2024-05-03 17:05:16 +0200

    Add julia_*() helper functions

--- R/julia-helpers.R
+++ R/julia-helpers.R
@@ -1,7 +1,19 @@
-# Print an R object in Julia
-julia_print <- function(x) {
+# Glimpse an R object in Julia
+julia_glimpse <- function(x) {
   julia_assign("x", x)
   julia_command("println(x);")
+  nothing()
+}
+
+# Print an object in Julia
+julia_print <- function(x) {
+  julia_command(glue('println({x})'))
+  nothing()
+}
+
+# Print the summary of object in Julia
+julia_summary <- function(x) {
+  julia_command(glue('summary({x})'))
   nothing()
 }
 
@@ -10,4 +22,15 @@ julia_timeline <- function(x) {
   check_inherits(x, "POSIXct")
   x <- check_tz(x)
   as.POSIXct(format(x, "%Y-%m-%d %H:%M:%S"), tz = lubridate::tz(x))
+}
+
+# Check if Julia object(s) have been set
+julia_check_exists <- function(...) {
+  x <- list(...)
+  lapply(x, function(xi) {
+    if (!julia_exists(xi)) {
+      abort("'{xi}' does not exist in Julia.", .envir = environment())
+    }
+  })
+  nothing()
 }
