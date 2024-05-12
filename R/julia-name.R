38c866c3d0eeac56bf699b2b214ed516c3e4726f
edwardlavender <edward.lavender@eawag.ch>
2024-05-12 12:03:33 +0200

    Add name_particles() function

---
+++ R/julia-name.R
@@ -0,0 +1,16 @@
+
+name_particles <- function(.fun = c("pf_filter", "pf_smoother_two_filter"),
+                           .direction = c("forward", "backward")) {
+  .fun       <- match.arg(.fun)
+  .direction <- match.arg(.direction)
+  if (.fun == "pf_filter" && .direction == "forward") {
+    # particles from forward run
+    "pfwd"
+  } else if (.fun == "pf_filter" && .direction == "backward") {
+    # particles from backward run
+    "pbwd"
+  } else if (.fun == "pf_smoother_two_filter") {
+    # particles from two-filter smoother
+    "ptf"
+  }
+}
