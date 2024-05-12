d71a2aef92f83bd5348499ae7f6f7dd85d5fc5a0
edwardlavender <edward.lavender@eawag.ch>
2024-05-12 12:06:36 +0200

    Add pf_smoother_two_filter()

---
+++ R/particle-smoother.R
@@ -0,0 +1,42 @@
+# Two-filter smoother
+pf_smoother_two_filter <- function(.map = NULL,
+                                   .mobility = .map,
+                                   .nMC = 100L,
+                                   .verbose = getOption("patter.verbose")) {
+
+  #### Set up messages
+  t_onset <- Sys.time()
+  cat_log <- cat_init(.verbose = .verbose)
+  cat_log(call_start(.fun = "pf_smoother_two_filter", .start = t_onset))
+  on.exit(cat_log(call_end(.fun = "pf_smoother_two_filter", .start = t_onset, .end = Sys.time())), add = TRUE)
+
+  #### Define smoother arguments
+  cat_log(paste0("... ", call_time(Sys.time(), "%H:%M:%S"), ": Setting smoother arguments..."))
+  # two_filter_smoother(xfwd, xbwd, move, box, nMC)
+  # * xfwd, xbwd and move exist in Julia
+  # * box and nMC are defined below
+
+  #### (optional) Define mobility box
+  if (!is.null(.mobility) & !is.null(.map)) {
+    # `.mobility` can be provided to define a box within which movements are always valid for 2D states
+    pf_obj <- name_particles(.fun = "pf_filter", .direction = "forward")
+    .state <- as.character(julia_eval(glue('typeof({pf_obj}.state[1]);')))
+    if (.state != "StateXY") {
+      warn(".mobility implemented but the State is not StateXY.")
+    }
+    # Define the box in Julia
+    set_mobility_box(spatMobilityBox(.map, .mobility = .mobility))
+  }
+
+  #### Run the smoother
+  cat_log(paste0("... ", call_time(Sys.time(), "%H:%M:%S"), ": Running smoother..."))
+  pf_obj <- set_smoother_two_filter(.nMC)
+
+  #### Get particles in R
+  # * TO DO: amend Julia code
+  # * TO DO: generalise pf_filter() R-side code
+  # cat_log(paste0("... ", call_time(Sys.time(), "%H:%M:%S"), ": Collating outputs..."))
+  # out <- julia_eval(glue('Patter.r_get_particles({pf_obj});'))
+  # out
+
+}
