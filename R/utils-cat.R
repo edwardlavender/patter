aa0179d7ea498d4dab1ad1992830c46533e3ca25
edwardlavender <edward.lavender@eawag.ch>
2024-05-12 13:59:15 +0200

    Add cat_setup() function

--- R/utils-cat.R
+++ R/utils-cat.R
@@ -1,21 +1,27 @@
 #' @title Utilities: `cat_*()` functions
 #' @description `cat_*()` functions control user output in [`patter`] functions with the `.verbose` argument.
+#' @param .fun A `character` string that defines the name of the parent function.
 #' @param .verbose A `logical` variable or a `string` that defines the path to a text file.
 #' * `.verbose = FALSE` suppresses user outputs;
 #' * `.verbose = TRUE` sends user outputs to the console;
-#' * `.verbose = file.path("path/to/text/file.txt")` sends user outputs to a `.txt` file;
+#' * `.verbose = file.path("path", "to", "text", "file.txt")` sends user outputs to a `.txt` file;
 #'
 #' @details These are internal functions.
+#' * [`cat_setup()`] sets up messages within functions;
+#' * [`cat_init()`] defines a [`cat()`] wrapper based on `.verbose`;
 #' * [`cat_log_file()`] validates `.verbose` and, if necessary, creates the `.txt` file;
-#' * [`cat_init()`] wraps [`cat_log_file()`] and defines an appropriate [`cat()`] wrapper for use in [`patter`] function(s) according to the input to `.verbose`;
 #'
 #' @examples
 #' \dontrun{
+#'
 #' # Define example function
-#' wrap <- function(.verbose) {
-#'   cat_log <- cat_init(.verbose)
-#'   cat_log("Hello world")
-#'   invisible(NULL)
+#' wrap <- function(.verbose = getOption("Patter.verbose")) {
+#'   # Set up messages and exit handler
+#'   cats <- cat_setup(.fun = "wrap", .verbose = .verbose)
+#'   on.exit(eval(cats$exit, envir = cats$envir), add = TRUE)
+#'   # Run function
+#'   cats$cat("Running function...")
+#'   Sys.sleep(1)
 #' }
 #'
 #' # `.verbose = TRUE` sends user output to the console
@@ -32,6 +38,41 @@
 #' }
 #' @author Edward Lavender
 #' @name cat_
+
+#' @rdname cat_
+#' @keywords internal
+
+# Set up messages for a function
+cat_setup <- function(.fun, .verbose) {
+  # Define start time
+  t_onset <- Sys.time()
+  # Define cat_log() function
+  cat_log <- cat_init(.verbose = .verbose)
+  # Provide startup message
+  cat_log(call_start(.fun = .fun, .start = t_onset))
+  # Define exit expression
+  exit_expr <- expression(
+    cat_log(call_end(.fun = .fun, .start = t_onset, .end = Sys.time()))
+  )
+  # Define a list of outputs
+  list(cat = cat_log,
+       exit = exit_expr,
+       envir = environment())
+}
+
+#' @rdname cat_
+#' @keywords internal
+
+# Initiate cat() options and get an appropriate cat() function
+cat_init <- function(.verbose) {
+  # Define log file
+  log_file <- cat_log_file(.verbose = .verbose)
+  # Define function to send messages to console or file
+  append_messages <- ifelse(log_file == "", FALSE, TRUE)
+  function(..., message = !isFALSE(.verbose), file = log_file, append = append_messages) {
+    if (message) cat(paste(..., "\n"), file = file, append = append)
+  }
+}
 
 #' @rdname cat_
 #' @keywords internal
@@ -75,19 +116,6 @@ cat_log_file <- function(.verbose) {
   .verbose
 }
 
-#' @rdname cat_
-#' @keywords internal
-
-# Initiate cat() options and get an appropriate cat() function
-cat_init <- function(.verbose) {
-  # Define log file
-  log_file <- cat_log_file(.verbose = .verbose)
-  # Define function to send messages to console or file
-  append_messages <- ifelse(log_file == "", FALSE, TRUE)
-  function(..., message = !isFALSE(.verbose), file = log_file, append = append_messages) {
-    if (message) cat(paste(..., "\n"), file = file, append = append)
-  }
-}
 
 #' @title Utilities: strings
 #' @name utils-str
