be18e8dc2b1d36b4cf47ea1a0b7329947bd69490
edwardlavender <edward.lavender@eawag.ch>
2024-05-20 11:32:09 +0200

    As above

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -23,3 +23,14 @@ char_to_class <- function(.x) {
 # Convert a `character` vector to a list of classes for dispatch
 # (e.g., c("ModelObsAcousticLogisTrunc", "ModelObsDepthUniform"))
 chars_to_classes <- Vectorize(char_to_class)
+
+#' @title Utilities: clean up
+#' @description This function deletes temporary files and or directories recursively.
+#' @details This function is a simple wrapper for [`unlink`] that is used at the end of examples.
+#' @author Edward Lavender
+#' @export
+
+cleanup <- function(x) {
+  unlink(x, recursive = TRUE)
+  nothing()
+}
