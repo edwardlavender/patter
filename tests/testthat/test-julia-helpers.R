12712d1ccb4c9c5a17cb19da158be051f59ce885
edwardlavender <edward.lavender@eawag.ch>
2024-06-12 11:15:08 +0200

    Add tests

---
+++ tests/testthat/test-julia-helpers.R
@@ -0,0 +1,57 @@
+test_that("Julia helpers work", {
+
+  check_inherits(julia_run(), "logical")
+
+  check_inherits(julia_skip(), "logical")
+
+  expect_true(julia_works())
+
+  # julia_proj_path()
+
+  # julia_proj_generate()
+
+  # julia_proj_activate()
+
+  # julia_proj_temp()
+
+  # julia_packages_dev_Patter.jl()
+
+  # julia_packages_install()
+
+  # julia_packages_library()
+
+  # julia_packages()
+
+  # julia_threads()
+
+  julia_glimpse(data.frame(x = 1))
+
+  julia_assign("x", 1)
+  julia_print("x")
+  julia_summary("x")
+
+  file <- tempfile(fileext = ".jld2")
+  julia_save("x", .file = file)
+  expect_true(file.exists(file))
+  julia_load(file)
+  expect_true(julia_exists("x"))
+
+  input <- dat_acoustics$timestamp
+  output <- julia_timeline(input)
+  expect_identical(input, output)
+
+  julia_check_exists("x")
+  julia_check_exists("blah") |>
+    expect_error("'bla' does not exist in Julia", fixed = TRUE)
+
+  julia_code(
+    '
+  x = 1
+  y = 2
+  z = x + y
+  '
+  )
+  expect_identical(julia_eval("z"), 3L)
+
+})
+
