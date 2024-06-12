109d652c1fcf4a54a392096bdf1b16199522dc96
edwardlavender <edward.lavender@eawag.ch>
2024-06-12 16:53:37 +0200

    Add tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,10 +2,15 @@
 julia <- julia_connect()
 
 # Define helper functions
+
 snapshot_png <- function(code, width = 400, height = 400) {
   path <- tempfile(fileext = ".png")
   png(path, width = width, height = height)
   on.exit(dev.off(), add = TRUE)
   eval(code)
   path
+}
+
+expect_approx <- function(output, truth, tolerance = 0.1) {
+  expect_true(abs(output - truth) < tolerance)
 }
---
+++ tests/testthat/test-skill.R
@@ -0,0 +1,26 @@
+test_that("skill_*() functions work", {
+
+  set.seed(1)
+
+  mod <- obs <- terra::rast()
+  n <- terra::ncell(mod)
+  mod[] <- runif(n)
+
+  obs[] <- mod[] - 10
+  expect_equal(skill_mb(mod, obs), -10)
+  expect_equal(skill_me(mod, obs), 10)
+  expect_equal(skill_rmse(mod, obs), 10)
+  expect_equal(skill_R(mod, obs), 1)
+  expect_equal(skill_d(mod, obs), -0.94990544)
+
+  mod <- obs <- terra::rast(res = 0.1)
+  n <- terra::ncell(mod)
+  mod[] <- runif(n)
+  obs[] <- mod[] + 0.5 * rnorm(n, mean = 100)
+
+  expect_approx(skill_mb(mod, obs), 50)
+  expect_approx(skill_me(mod, obs), 50)
+  expect_approx(skill_rmse(mod, obs), 50)
+  expect_approx(skill_R(mod, obs), 0.5)
+
+})
