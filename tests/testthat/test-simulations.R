fd011d53c8bc92c353f6e7e3edf42c180333e472
edwardlavender <edward.lavender@eawag.ch>
2024-10-21 16:18:54 +0200

    Add test for sim_path_walk()

--- tests/testthat/test-simulations.R
+++ tests/testthat/test-simulations.R
@@ -24,3 +24,32 @@ test_that("sim_array() works", {
   expect_equal(a$receiver_id, c(1, 2, 1, 2))
 
 })
+
+test_that("sim_path_walk() works", {
+
+  #### Set up test
+  timeline <- assemble_timeline(list(dat_detections), .step = "2 mins")[1:1000]
+  map      <- dat_gebco()
+  set_map(map)
+  set_seed()
+
+  #### Test error handling
+  # Expect error for incorrectly specified maps or models
+  sim_path_walk(.map = map,
+                .timeline = timeline,
+                .state = "StateXY",
+                .model_move = move_xy(dbn_angle = "VonMises(0, 100)")) |>
+    expect_error()
+
+  #### Test correct implementation
+  # Run function
+  path <- sim_path_walk(.map = map, .timeline = timeline)
+  # Check output properties
+  check_inherits(path, "data.table")
+  check_names(path, c("path_id", "timestep", "timestamp", "map_value", "x", "y"))
+  # Validate map_value()
+  expect_equal(path$map_value, terra::extract(map, cbind(path$x, path$y))$map_value)
+  # Validate movement distances
+  expect_true(all(terra::distance(cbind(path$x, path$y), lonlat = FALSE, sequential = TRUE) <= 750.0))
+
+})
