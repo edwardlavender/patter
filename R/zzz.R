61e67a8d250851f22a9cfbeee0974c992ae6723a
edwardlavender <edward.lavender@eawag.ch>
2024-10-22 10:28:20 +0200

    Add startup warning on linux

--- R/zzz.R
+++ R/zzz.R
@@ -17,5 +17,10 @@
 }
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("This is {patter} v.", utils::packageVersion("patter"), ". For an overview, see `?patter`. For support, contact edward.lavender@eawag.ch."))
+  msg_startup <- paste0("This is {patter} v.", utils::packageVersion("patter"), ". For an overview, see `?patter`. For support, contact edward.lavender@eawag.ch.")
+  msg_linux   <- ""
+  if (grepl("linux", tolower(Sys.info()["sysname"]))) {
+    msg_linux   <- "WARNING: On Linux, {patter} can be used but you cannot simultaneously use geospatial routines in R and Julia. Thus, you can only call `library(terra)` or `terra::foo()` and use {patter} routines that exploit {terra} and other geospatial packages in R sessions that are not connected to a Julia session (via `julia_connect()`). Set the map in Julia using a file path (via `set_map()`). Check the {patter} function documentation for supported options."
+  }
+  packageStartupMessage(paste(msg_startup, msg_linux))
 }
